213ed9efd727367034705cc513b47c8029f7a639
Vishal Aiely <vishalaiely@Vishals-MacBook-Pro.local>
2020-07-04 02:15:29 -0500

    Audio files added game detects move and makes them Stockfish responds with best move

--- audio.py
+++ audio.py
@@ -2,11 +2,21 @@ import os
 import time
 import playsound
 import speech_recognition as sr
+import chess
 from gtts import gTTS
+
+PieceDict = {
+    'Knight' : 'N',
+    'Bishop' : 'B',
+    'Rook' : 'R',
+    'Queen' : 'Q',
+    'King' : 'K',
+    'Pawn' : ''
+}
 
 def speak(text):
     tts = gTTS(text=text, lang='en')
-    filename = "temp.mp3"
+    filename = "audio/temp.mp3"
     tts.save(filename)
     playsound.playsound(filename)
 
@@ -18,8 +28,16 @@ def get_audio():
 
         try:
             spoken = r.recognize_google(audio)
-            print(spoken)
+            #print(spoken)
         except Exception as e:
             print("Exception: " + str(e))
 
     return spoken
+
+#Gets the move and converts it to the proper format
+def get_move():
+    playerSaid = "".join(get_audio().lower().split())
+
+    #print(f'I heard: {playerSaid.strip()}')
+
+    return playerSaid.strip()
--- main.py
+++ main.py
@@ -1,12 +1,87 @@
-from audio import speak, get_audio
+from audio import speak, get_audio, get_move
 import chess
 import chess.svg
+import playsound
+from stockfish import Stockfish
+from datetime import datetime
+import random
+
+def playermove(boardstate):
+    move = get_move()
+
+    #print(f"Player move: {move}")
+    #print(boardstate.legal_moves)
+
+    if 'quit' in move:
+        speak('Thanks for playing, here is the board')
+        print(boardstate)
+        exit()
+
+    while True:
+        try:
+            if chess.Move.from_uci(move) in boardstate.legal_moves:
+                return move
+            else:
+                playsound.playsound('audio/invalid.mp3')
+        except:
+            playsound.playsound('audio/nohear.mp3')
+            print(f'I heard: {move}')
+
+
+        move = get_move()
+
+        if 'quit' in move:
+            speak('Thanks for playing, here is the board')
+            print(boardstate)
+            exit() 
+    
+    
+    return move
 
 def main():
+    #Deteremine start
+    random.seed(datetime.now())
+    white = random.randint(0,1)
+
+    #initialize board and stockfish
     board = chess.Board()
-    print(board)
-    board.push_san('e4')
-    print(board)
+    fishy = Stockfish('/Users/vishalaiely/Downloads/stockfish-11-mac/Mac/stockfish-11-64')
+
+    if white:
+        playsound.playsound('audio/WhiteStart.mp3')
+    else:
+        playsound.playsound('audio/BlackStart.mp3')
+
+    while not board.is_game_over():
+        if white:
+            board.push_uci(playermove(board))
+
+            fishy.set_fen_position(board.fen())
+            compMove = fishy.get_best_move()
+            board.push_uci(compMove)
+
+            speak(compMove)
+
+        else:
+           fishy.set_fen_position(board.fen())
+           compMove = fishy.get_best_move()
+           board.push_uci(compMove)
+
+           speak(compMove)
+
+           board.push_uci(playermove(board))
+
+    if board.result() is '1-0':
+        playsound.playsound('audio/WhiteWin.mp3')
+    elif board.reset() is '0-1':
+       playsound.playsound('audio/BlackWin.mp3')
+    elif board.is_stalemate():
+        speak('Stalemate')
+    else:
+        speak('Draw')
+
+    speak('Thank you for playing!')
+    
 
 if __name__ == '__main__':
     main()
